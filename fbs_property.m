f14fd9b1f2a93ac84585ba43aef74ab623f156cc
rich86man <richardbkirk@gmail.com>
2014-12-10 12:50:41 -0800

    added fbs property

---
+++ fbs_property.m
@@ -0,0 +1,7 @@
+// FBS Property Block
+// Fucking Block Syntax - Property Block
+//
+// IDECodeSnippetCompletionPrefix: fbs-property
+// IDECodeSnippetCompletionScopes: [TopLevel]
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+@property (nonatomic, copy) <#returnType#> (^<#blockName#>)(<#parameterTypes#>);
